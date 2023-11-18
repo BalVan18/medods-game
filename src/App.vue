a9cfaccf6f53cbd6450975a4b3e44032155fee67
Ivan Balakin <ivanbal1821@yandex.ru>
2023-11-18 18:58:08 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SimonTheGame />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SimonTheGame from './components/SimonTheGame.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SimonTheGame
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+  @import '@/assets/style/main'
 </style>
